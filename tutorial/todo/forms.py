f8a3218df5bf21fa5da3f91f907f65577e58c9d6
Parrot User <user@localhost.localdomain>
2020-12-15 03:32:12 +0530

    add completed

---
+++ tutorial/todo/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.forms import ModelForm
+
+from .models import Tasks
+
+# class AddTaskForm(forms.Form):
+# 	text = forms.CharField(max_length = 250,
+
+# 							widget = forms.TextInput(
+# 								attrs = {
+# 									'class' : 'form-control',
+# 									'placeholder' : 'task?'
+# 								}
+# 							)
+# 						)
+
+class AddTaskForm(forms.ModelForm):
+
+	task = forms.CharField(max_length = 250,
+							widget = forms.TextInput(
+								attrs = {
+									'class' : 'form-control',
+									'placeholder' : 'task?'
+								}
+							)
+						)
+
+	class Meta:
+		model = Tasks
+		fields = '__all__'
+
+
+		
+								
+
+
--- tutorial/todo/views.py
+++ tutorial/todo/views.py
@@ -1,14 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Tasks
+from .forms import AddTaskForm
 
 # Create your views here.
 def index(request):
 
     tasks = Tasks.objects.all()
+    form = AddTaskForm()
 
     context = {
         'tasks' : tasks,
+        'form' : form,
     }
 
     return render(request, 'todo/index.html', context)
+
+def addTask(request):
+
+	form = AddTaskForm(request.POST)
+
+	if form.is_valid():
+		form.save()
+
+	return redirect('/')
+
+
+    
